849cbca3ec33b7df342d3f33fe06795b23e514a0
edroque93 <quique.lpgc@gmail.com>
2018-02-04 21:03:15 +0100

    Let's ease things

--- src/notes.h
+++ src/notes.h
@@ -11,3 +11,12 @@ typedef struct gtkNote {
   uint32_t width, height;
   uint32_t x, y;
 } gtkNote;
+
+typedef struct gtkNoteArray {
+  uint32_t length;
+  gtkNote **array;
+} gtkNoteArray;
+
+void freeGTKNoteArray(gtkNoteArray *f);
+void freeGTKNote(gtkNote *f);
+void freeNote(note *f);
