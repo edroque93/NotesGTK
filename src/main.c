f6423ad22df7e17f525d7f09482098656c0814f1
edroque93 <quique.lpgc@gmail.com>
2018-02-06 20:47:31 +0100

    Proper main

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include "database.h"
+#include "notes.h"
 #include "stdio.h"
 
 int main(int argc, char **argv) {
-  notesDB db;
+  notesDB db = {.errFunc = "", .errMsg = ""};
 
   if (!initDB(&db)) {
     printLastDBError(&db);
   } else {
+    gtkNoteArray array = loadNotes(&db);
+    printGTKNoteArray(&array);
+    freeGTKNoteArray(&array);
     closeDB(&db);
   }
 
